8d18885a5cece7195d33a5b73240f542974e21b3
suzukiyuya <mcd2376022@stu.o-hara.ac.jp>
2025-05-20 14:55:47 +0900

    オリジナルを廃棄、コピーを生成

--- Baet_eat/Assets/Suzuki/Fonts/SelectSceneFontSet.cs
+++ Baet_eat/Assets/Suzuki/Fonts/SelectSceneFontSet.cs
@@ -13,25 +13,33 @@ public class SelectSceneFontSet : MonoBehaviour
     private TextMeshProUGUI _text=new();
     private List<GameObject> gameObjects = new(MusicManager.CAPACITY);
 
+    [SerializeField] private TMP_FontAsset _fontAsset;
+    [SerializeField] private Texture2D _fontTexture;
+
+    [SerializeField] private TMP_FontAsset _CheckfontAsset;
+
     private void Awake()
     {
-        gameObjects=MusicManager.instance.GetMusicCards();
-        for(int i = 0; i < MusicManager.CAPACITY; i++)
+        _fontAsset.atlas=_fontTexture;
+        Debug.Log(_CheckfontAsset);
+
+        gameObjects = MusicManager.instance.GetMusicCards();
+        for (int i = 0; i < MusicManager.CAPACITY; i++)
         {
             // スコアテキストのマテリアル変更
             GameObject gameObject = gameObjects[i].transform.GetChild(1).gameObject;
             // スコアの数値
-            _text=gameObject.GetComponent<TextMeshProUGUI>();
+            _text = gameObject.GetComponent<TextMeshProUGUI>();
             _text.fontMaterial = rankAndScoreFontMaterial;
             // 「Score:」
-            _text=gameObject.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+            _text = gameObject.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
             _text.fontMaterial = rankAndScoreFontMaterial;
 
             // クリアランクのマテリアル変更
             gameObject = gameObjects[i].transform.GetChild(6).gameObject;
             // プラスマークのアウトライン
             _text = gameObject.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
-            _text.fontMaterial= rankAndScoreFontMaterial;
+            _text.fontMaterial = rankAndScoreFontMaterial;
             // プラスマーク
             _text = gameObject.transform.GetChild(0).transform.GetChild(0).GetComponent<TextMeshProUGUI>();
             _text.fontMaterial = _nomalFontMaterial;
@@ -46,9 +54,9 @@ public class SelectSceneFontSet : MonoBehaviour
             _text.fontMaterial = rankAndScoreFontMaterial;
 
             // 難易度マテリアル設定
-            if(i>_difficultys.Count-1) return;
+            if (i > _difficultys.Count - 1) return;
             _text = _difficultys[i].transform.GetChild(1).GetComponent<TextMeshProUGUI>();
-            _text.fontMaterial=_difficultyFontMaterial;
+            _text.fontMaterial = _difficultyFontMaterial;
         }
     }
 }
