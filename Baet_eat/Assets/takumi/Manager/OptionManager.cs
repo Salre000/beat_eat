1b970fa3d0171407ccc2a6ec6eec434a038a7560
Salre000 <mcd2376019@stu.o-hara.ac.jp>
2025-05-20 16:20:45 +0900

    ページを分ける処理を作成

--- Baet_eat/Assets/takumi/Manager/OptionManager.cs
+++ Baet_eat/Assets/takumi/Manager/OptionManager.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class OptionManager : MonoBehaviour
 {
     [SerializeField]SliderVolume _sliderVolume;
+
+    private int nowPage = 0;
+
+    private List<GameObject> PageList = new List<GameObject>();
     private void Start()
     {
         OptioStatus.Initialize();
         SetOptionData();
 
+        for (int i = 0; i < _sliderVolume.gameObject.transform.childCount; i++) 
+            PageList.Add(_sliderVolume.gameObject.transform.GetChild(i).gameObject);
+        NowPageShow();
     }
 
     public void SetOptionData() 
@@ -18,4 +25,22 @@ public class OptionManager : MonoBehaviour
         _sliderVolume.SetSE(OptioStatus.GetSE_Volume());
 
     }
+    private void NowPageShow() 
+    {
+
+        for(int i = 0; i < PageList.Count; i++) 
+        {
+            PageList[i].SetActive(false);
+
+            if (i != nowPage * 2 && i != nowPage + 1) continue;
+
+            PageList[i].SetActive(true);
+
+
+        }
+
+
+    }
+    public void AddPage() { nowPage += 1;if (nowPage > 2) nowPage = 2; NowPageShow(); }
+    public void SbuPage() { nowPage -= 1; if (nowPage < 0) nowPage = 0; NowPageShow(); }
 }
