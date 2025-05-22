a11c9d8cab0de492c7ead3360ed00013bc039a3f
TMIC-GAME3-006\student1 <mcd2376083@stu.o-hara.ac.jp>
2025-05-22 12:37:24 +0900

    スコアラインの作成

--- Baet_eat/Assets/Suzuki/Script/MainScene/SetFontMaterial.cs
+++ Baet_eat/Assets/Suzuki/Script/MainScene/SetFontMaterial.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 
 public class SetFontMaterial : MonoBehaviour
 {
+    // 現在のスコア
     [SerializeField] TextMeshProUGUI _scoreText;
+    // 読んで字の如く「Life」
     [SerializeField] List<TextMeshProUGUI> _lifeText;
+    // 現在のスコアランク
     [SerializeField] List<TextMeshProUGUI> _rankText;
+    // ゲージラインのランク
+    [SerializeField] List<TextMeshProUGUI> _lineRank;
+    // 白アウトライン
     [SerializeField]Material _scoreMaterial;
+    // 灰アウトライン
     [SerializeField]Material _lifeMaterial;
+    // 黒アウトライン
     [SerializeField]Material _rankMaterial;
 
     // Start is called before the first frame update
@@ -22,6 +30,10 @@ public class SetFontMaterial : MonoBehaviour
         for(int i=0;i< _rankText.Count;i++)
         {
             _rankText[i].fontMaterial = _rankMaterial;
+        }
+        for(int i=0;i< _lineRank.Count;i++)
+        {
+            _lineRank[i].fontMaterial = _lifeMaterial;
         }
             _scoreText.fontMaterial = _scoreMaterial;
     }
