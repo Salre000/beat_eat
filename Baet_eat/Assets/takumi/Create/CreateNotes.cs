795276f56f27e2a73d0242d14818f469fefb4a49
SALRE000\tashi <Salre>
2025-05-24 11:38:05 +0900

    ノーツの大きくなる向きを修正

--- Baet_eat/Assets/takumi/Create/CreateNotes.cs
+++ Baet_eat/Assets/takumi/Create/CreateNotes.cs
@@ -115,7 +115,7 @@ public class CreateNotes : MonoBehaviour
             // éûä‘Å@ kankaku * inputJson.notes[i].num 
 
 
-            notes.transform.position = new Vector3((inputJson.notes[i].block) - 3.5f - (float)inputJson.notes[i].renge / 2.0f, 0.03f,( kankaku * inputJson.notes[i].num * InGameStatus.GetSpeed() * 20)+offset);
+            notes.transform.position = new Vector3((inputJson.notes[i].block) - 4.5f + (float)inputJson.notes[i].renge / 2.0f, 0.03f,( kankaku * inputJson.notes[i].num * InGameStatus.GetSpeed() * 20)+offset);
             //êeÇ…ìZÇﬂÇÈ
             notes.transform.parent = NotesParent.transform;
 
