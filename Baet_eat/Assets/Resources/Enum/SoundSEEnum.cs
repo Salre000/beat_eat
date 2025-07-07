aa1f782532aac1d4dd93ec88bdc822777a5fc38a
TMIC-GAME3-006\student1 <mcd2376083@stu.o-hara.ac.jp>
2025-07-07 16:25:26 +0900

    SEの順番

--- Baet_eat/Assets/Resources/Enum/SoundSEEnum.cs
+++ Baet_eat/Assets/Resources/Enum/SoundSEEnum.cs
@@ -1,5 +1,7 @@
 public  static class SoundSEEnum {
 public enum SoundSEType {
+        /// <summary><see _Drum="ドラム"/> </summary>\r\n
+_Drum,
         /// <summary><see _normal="ごく一般的な音"/> </summary>\r\n
 _normal,
 MAX
