51c7d53a76fb3d84d734b3512394de2207c87239
TMIC-GAME3-006\student1 <mcd2376083@stu.o-hara.ac.jp>
2025-07-16 12:28:55 +0900

    丸ノーツ修正

--- Baet_eat/Assets/Resources/Enum/NotesMaterialTypeEnum.cs
+++ Baet_eat/Assets/Resources/Enum/NotesMaterialTypeEnum.cs
@@ -2,8 +2,8 @@ public  static class NotesMaterialTypeEnum {
 public enum NotesMaterialType {
         /// <summary><see _normal="ごく一般的な物"/> </summary>\r\n
 _normal,
-        /// <summary><see _Debugnormal="ごく一般的な物デバッグよう"/> </summary>\r\n
-_Debugnormal,
+        /// <summary><see _circle="円の形"/> </summary>\r\n
+_circle,
 MAX
 }
 }
