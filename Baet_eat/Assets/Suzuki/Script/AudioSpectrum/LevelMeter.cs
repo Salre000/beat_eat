e776bacb5c6a55548d772f38b4f8fba8afba295b
TMIC-GAME3-006\student1 <mcd2376083@stu.o-hara.ac.jp>
2025-07-17 09:41:45 +0900

    オーディオレベルメーターの不要な変数削除

--- Baet_eat/Assets/Suzuki/Script/AudioSpectrum/LevelMeter.cs
+++ Baet_eat/Assets/Suzuki/Script/AudioSpectrum/LevelMeter.cs
@@ -5,10 +5,10 @@ using UnityEngine.UI;
 
 public class LevelMeter : MonoBehaviour
 {
-    [SerializeField] private AudioSpectrum spectrum;
-    [SerializeField] private List<Transform> objects;
-    [SerializeField] private float scale;
-    private List<Image> images;
+    [SerializeField] private AudioSpectrum _spectrum;
+    [SerializeField] private List<Transform> _objects;
+    [SerializeField] private float _scale;
+    private List<Image> _images;
     private Transform _transform = null;
     private Vector3 _localScale = new Vector3(0, 0, 0);
     Vector3 _currentScale = Vector3.one;
@@ -18,10 +18,10 @@ public class LevelMeter : MonoBehaviour
 
     private void Awake()
     {
-        images = new(objects.Count);
-        for (int i = 0; i < objects.Count; i++)
+        _images = new(_objects.Count);
+        for (int i = 0; i < _objects.Count; i++)
         {
-            images.Add(objects[i].GetComponent<Image>());
+            _images.Add(_objects[i].GetComponent<Image>());
         }
         StartCoroutine(WaitFrame());
     }
@@ -44,11 +44,11 @@ public class LevelMeter : MonoBehaviour
 
     private void Meter()
     {
-        for (int i = 0; i<objects.Count; i++)
+        for (int i = 0; i<_objects.Count; i++)
         {
-            _transform = objects[i];
+            _transform = _objects[i];
             _localScale = _transform.localScale;
-            _localScale.y = spectrum.Levels[i]* scale;
+            _localScale.y = _spectrum.Levels[i]* _scale;
             if (_localScale.y <= 0.1f) _localScale.y = 0.1f;
             _currentScale = _transform.localScale;
             _currentScale.y = Mathf.Lerp(_currentScale.y, _localScale.y, Time.deltaTime * 150);
@@ -61,24 +61,24 @@ public class LevelMeter : MonoBehaviour
     private void ChangeColor()
     {
         if (MusicManager.instance.IsChangeDifficulty()) return;
-        for (int i = 0; i < objects.Count; i++)
+        for (int i = 0; i < _objects.Count; i++)
         {
             switch (MusicManager.instance.GetDifficultyNumber())
             {
                 case 0:
-                    images[i].color = ColorManager.DRINK_COLOR_TRANSLUCENT;
+                    _images[i].color = ColorManager.DRINK_COLOR_TRANSLUCENT;
                     break;
                 case 1:
-                    images[i].color = ColorManager.HORSDOEUVRE_COLOR_TRANSLUCENT;
+                    _images[i].color = ColorManager.HORSDOEUVRE_COLOR_TRANSLUCENT;
                     break;
                 case 2:
-                    images[i].color = ColorManager.SOUP_COLOR_TRANSLUCENT;
+                    _images[i].color = ColorManager.SOUP_COLOR_TRANSLUCENT;
                     break;
                 case 3:
-                    images[i].color = ColorManager.MAINDISH_COLOR_TRANSLUCENT;
+                    _images[i].color = ColorManager.MAINDISH_COLOR_TRANSLUCENT;
                     break;
                 case 4:
-                    images[i].color = ColorManager.DESSERT_COLOR_TRANSLUCENT;
+                    _images[i].color = ColorManager.DESSERT_COLOR_TRANSLUCENT;
                     break;
             }
         }
